70bce3beff4722b1b49ce6746f39bb5ed95e8e42
Juan Gomez <jf.gomezramirez@gmail.com>
2022-08-13 18:02:30 -0700

    Contact section changed, now the form works. Footer changed, links updated

---
+++ index.js
@@ -0,0 +1,46 @@
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "100%";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
+
+/* CONTACT FORM */
+let inp = document.getElementsByTagName('input');
+let textInp = document.querySelector('textarea');
+
+const submitForm = () => {
+  fetch("https://formsubmit.co/ajax/gomezjuanefe@gmail.com", {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      name: inp.name.value,
+      email: inp.email.value,
+      subject: inp.subject.value,
+      message: textInp.value
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      alert('Tu mensaje se ha recibido satisfactoriamente');
+      inp.name.value = '';
+      inp.email.value = '';
+      inp.subject.value = '';
+      textInp.value = '';
+    })
+    .catch(error => {
+      alert('Algo salió mal, inténtalo más tarde.');
+    });
+}
+
+let submitBtn = document.querySelector('input[type="submit"]');
+submitBtn.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  submitForm();
+})
+
